21fc3f8a3db032e60c9d58e6088e828e97018320
Vitoria Silva <vitoriaconsyl@gmail.com>
2026-04-01 16:02:39 -0300

    alteração

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import Header from "./components/Header";
 import Sobre from "./components/Sobre";
 import Projetos from "./components/Projetos";
 import Contato from "./components/Contato";
-
+import fundo from "./assets/fund.jpg";
 
 function App() {
   return (
-    <>
+    <div style={{
+      backgroundImage: `url(${fundo})`,
+      backgroundSize: "cover"
+    }}>
       <Header />
       <Sobre />
       <Projetos />
       <Contato />
-    </>
+    </div>
   );
 }
 
--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -1,10 +1,12 @@
+import perfil from "../assets/perfil.jpeg";
+
 function Sobre() {
     return (
         <section id="sobre">
 
             <h2>Sobre mim</h2>
 
-            <img src="src/assets/perfil.jpeg" alt="Foto de Vitória Silva" />
+            <img src={perfil} alt="Foto de Vitória Silva" />
 
             <p>Tenho formação técnica em Informática e atualmente curso Tecnologia em Sistemas para Internet.
                 Atuo como professora no curso técnico de Desenvolvimento de Sistemas no ensino médio.
